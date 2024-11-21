89c7e9a48754b7c7cfcef126f7c607935792944d
Arief Novanda Zulvan <arief.novanda.zulvan@gmail.com>
2024-11-22 02:13:34 +0700

    Feat : innit boiler template

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hyperhire</title>
+        <meta name="description" content="Hyperhire hiring platform" />
+      </Head>
+      <main className="bg-gradient-to-br from-cyan-500 to-blue-500 text-white min-h-screen">
+      
+      </main>
+    </>
+  );
+}
